d9f4455fa06cc3f81c5b0480636682563a47aa9b
ChrisBentley <c.j.bentley90@gmail.com>
2016-12-08 13:34:44 +0000

    Program now completes the provided queries

--- fuzzy_search.py
+++ fuzzy_search.py
@@ -11,6 +11,7 @@ import argparse
 import os
 import sys
 import csv
+from fuzzywuzzy import fuzz
 
 class Product(object):
 
@@ -18,6 +19,9 @@ class Product(object):
         self.id = data[0]
         self.name = data[1]
         self.brand = data[2]
+
+    def set_score(self, score):
+        self.score = score
 
 
 def check_file_exists(file):
@@ -36,6 +40,49 @@ def parse_csv(csv_file):
             products.append(Product(item))
 
     return products
+
+
+def parse_queries(queries_file):
+
+    with open(os.path.abspath(queries_file)) as queriesfile:
+        queries = queriesfile.readlines()
+
+    return queries
+
+def fuzzy_search(query, products_list):
+
+    # split the query into a list of tokens
+    all_tokens = query.split(' ')
+    main_token = all_tokens[-1]
+
+    # remove the main_token fom the list of tokens
+    all_tokens.pop()
+
+    prefix_tokens = all_tokens
+
+    main_token_matches = []
+
+    for product in products_list:
+        product_string = product.name + ' ' + product.brand
+        if fuzz.token_set_ratio(main_token, product_string) == 100:
+            main_token_matches.append(product)
+
+    matching_products = []
+
+    for product in main_token_matches:
+        product_string = product.name + ' ' + product.brand
+        match = fuzz.token_set_ratio(prefix_tokens, product_string)
+        score = fuzz.token_sort_ratio(prefix_tokens, product_string)
+        if match == 100:
+            product.set_score(score)
+            matching_products.append(product)
+
+    sorted_matching_products = sorted(matching_products, key=lambda x: x.score, reverse=True)
+
+    if len(sorted_matching_products) > 10:
+        sorted_matching_products = sorted_matching_products[:10]
+
+    return sorted_matching_products
 
 
 def main():
@@ -59,8 +106,18 @@ def main():
 
     products_list = parse_csv(args.csv)
 
-    
+    queries = parse_queries(args.queries)
 
+    for query in queries:
+
+        query_result = fuzzy_search(query, products_list)
+
+        print('Query: ' + query)
+
+        for result in query_result:
+            print(str(result.score) + ',' + result.id + ',' + result.name + ',' + result.brand)
+
+        print('\n')
 
 
 (__name__ == '__main__' and main())
